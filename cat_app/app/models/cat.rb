622319c3824b43e441e02ba9d5ddc11d15a7811f
jackcusick95 <jackcusick95@gmail.com>
2021-04-02 14:38:23 -0400

    saved

--- cat_app/app/models/cat.rb
+++ cat_app/app/models/cat.rb
@@ -5,4 +5,9 @@ class Cat < ApplicationRecord
 
     validates :color, inclusion: { in: COLORS}
     validates :sex, inclusion: { in: ["M","F"] }
+    validates_presence_of :color, :birth_date, :name, :sex, :description
+
+    def age
+        time_ago_words(self.birth_date)
+    end 
 end 
